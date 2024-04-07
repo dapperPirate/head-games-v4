1d7d04a74dce654b66ca404d6cabe871233ba87e
JJS <55813477+dapperPirate@users.noreply.github.com>
2024-04-07 01:20:22 +0100

    Update headArrays.js

--- headArrays.js
+++ headArrays.js
@@ -2,5 +2,5 @@
 
 const jackArray = ["WitherFace", "HuskFace", "PufferfishFace", "AllayFace", "RavagerFace"];
 const alexArray = ["TurtleFace", "HuskFace", "PufferfishFace"];
-const bobbieArray = ["CreeperFace", "HuskFace", "SheepFace"];
+const bobbieArray = ["CreeperFace", "CowFace", "HuskFace", "SheepFace"];
 const charlieArray = ["CreeperFace", "SheepFace", "ElderGuardianFace"];
