68dc6b756d9211c7e43f84d88e2a616613d53f18
JJS <55813477+dapperPirate@users.noreply.github.com>
2024-04-12 22:28:59 +0100

    correct names

--- headArrays.js
+++ headArrays.js
@@ -1,4 +1,24 @@
-const alexArray = ["RabbitFace","WolfFace","ElderGuardianFace","SnowGolemFace","EnderdragonFace","SalmonBody","PigFace","ParrotFace","SquidFace","TropicalFishBody","VillagerFace","OcelotFace","CatFace","HorseFace","SheepFace","ZombieVillagerFace","DonkeyFace","ShulkerFace","CodFace","WanderingTraderFace","EvokerFace","VindicatorFace","BeeFace","ChickenFace","PhantomFace","MooshroomFace","PufferfishFace","HuskFace","IronGolemFace","TadpoleFace","TurtleFace","FoxFace","FrogFace","DolphinFace","StrayFace","CaveSpiderFace","PolarBearFace","PillagerFace","BatFace","PiglinBruteFace","StriderFace","GhastFace","MiniBlockFace","JebSheepFace","ZombifiedPiglinFace","LlamaFace","WitherSkeletonFace","AxolotlFace","PlayerFace","SilverfishFace","EndermanFace","EndermiteFace"];
-const bobbieArray = ["BeeFace","BatFace","HorseFace","BlazeFace","ParrotFace","SheepFace","CamelFace","CaveSpiderFace","ChickenFace","CodFace","DolphinFace","DonkeyFace","DrownedFace","EndermanFace","EvokerFace","HoglinFace","IronGolemFace","PandaFace","PigFace","PiglinBruteFace","PillagerFace","PolarBearFace","PufferfishFace","MooshroomFace","SalmonBody","RabbitFace","SilverfishFace","FoxFace","SpiderFace","SquidFace","TadpoleFace","TropicalFishBody","TurtleFace","VexFace","VillagerFace","WanderingTraderFace","FrogFace","CatFace","AxolotlFace","WolfFace","EnderdragonFace","CowFace","LlamaFace","ZombieVillagerFace","EndermiteFace","StrayFace","VindicatorFace","ElderGuardianFace","JebSheepFace","PlayerHead","MiniBlockFace"];
-const charlieArray = ["SheepFace","RabbitFace","CatFace","ParrotFace","OcelotFace","EvokerFace","WitchFace","IronGolemFace","AxolotlFace","PiglinBruteFace","VillagerFace","WitherFace","SilverfishFace","ZombieVillagerFace","PandaFace","EnderdragonFace","DonkeyFace","BeeFace","WolfFace","SquidFace","WanderingTraderFace","PufferfishFace","HorseFace","ChickenFace","PillagerFace","VindicatorFace","LlamaFace","CamelFace"];
+const alexArray = ["RabbitFace","WolfFace","ElderGuardianFace","SnowGolemFace",
+"EnderdragonFace","SalmonFace","PigFace","ParrotFace","SquidFace","TropicalFishFace",
+"VillagerFace","OcelotFace","CatFace","HorseFace","SheepFace","ZombieVillagerFace",
+"DonkeyFace","ShulkerFace","CodFace","WanderingTraderFace","EvokerFace","VindicatorFace",
+"BeeFace","ChickenFace","PhantomFace","MooshroomFace","PufferfishFace","HuskFace",
+"IronGolemFace","TadpoleFace","TurtleFace","FoxFace","FrogFace","DolphinFace","StrayFace",
+"CaveSpiderFace","PolarBearFace","PillagerFace","BatFace","PiglinBruteFace","StriderFace",
+"GhastFace","MiniBlock","JebSheepFace","ZombifiedPiglinFace","LlamaFace","WitherSkeletonFace",
+"AxolotlFace","PlayerFace","SilverfishFace","EndermanFace","EndermiteFace"];
+
+const bobbieArray = ["BeeFace","BatFace","HorseFace","BlazeFace","ParrotFace","SheepFace","CamelFace",
+"CaveSpiderFace","ChickenFace","CodFace","DolphinFace","DonkeyFace","DrownedFace","EndermanFace","EvokerFace",
+"HoglinFace","IronGolemFace","PandaFace","PigFace","PiglinBruteFace","PillagerFace","PolarBearFace",
+"PufferfishFace","MooshroomFace","SalmonFace","RabbitFace","SilverfishFace","FoxFace","SpiderFace","SquidFace",
+"TadpoleFace","TropicalFishFace","TurtleFace","VexFace","VillagerFace","WanderingTraderFace","FrogFace","CatFace",
+"AxolotlFace","WolfFace","EnderdragonFace","CowFace","LlamaFace","ZombieVillagerFace","EndermiteFace","StrayFace",
+"VindicatorFace","ElderGuardianFace","JebSheepFace","PlayerFace","MiniBlock"];
+
+const charlieArray = ["SheepFace","RabbitFace","CatFace","ParrotFace","OcelotFace","EvokerFace","WitchFace",
+"IronGolemFace","AxolotlFace","PiglinBruteFace","VillagerFace","WitherFace","SilverfishFace","ZombieVillagerFace",
+"PandaFace","EnderdragonFace","DonkeyFace","BeeFace","WolfFace","SquidFace","WanderingTraderFace","PufferfishFace",
+"HorseFace","ChickenFace","PillagerFace","VindicatorFace","LlamaFace","CamelFace"];
+
 const jackArray = [];
