31964034a4a6ad1dea2198d55cb987ce4ee320df
jtw26 <166263239+jtw26@users.noreply.github.com>
2024-04-12 11:56:35 +0100

    Update headArrays.js

--- headArrays.js
+++ headArrays.js
@@ -1,4 +1,4 @@
 const alexArray = [];
-const bobbieArray = ["WitherFace","BeeFace","BatFace","HorseFace","BlazeFace","ParrotFace","ParrotFace","SheepFace","PlayerFace","CamelFace","CaveSpiderFace","ChickenFace","CodFace"];
+const bobbieArray = ["WitherFace","BeeFace","BatFace","HorseFace","BlazeFace","ParrotFace","SheepFace","PlayerFace","CamelFace","CaveSpiderFace","ChickenFace","CodFace"];
 const charlieArray = [];
 const jackArray = [];
