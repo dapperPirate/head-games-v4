fa0b8feb3f3e5a29ab073596255237409695376b
jtw26 <166263239+jtw26@users.noreply.github.com>
2024-04-12 12:03:01 +0100

    Update headArrays.js

--- headArrays.js
+++ headArrays.js
@@ -1,4 +1,4 @@
 const alexArray = [];
-const bobbieArray = ["WitherFace","BeeFace","BatFace","HorseFace","BlazeFace","ParrotFace","SheepFace","PlayerFace","CamelFace","CaveSpiderFace","ChickenFace","CodFace"];
+const bobbieArray = ["WitherFace","BeeFace","BatFace","HorseFace","BlazeFace","ParrotFace","SheepFace","CamelFace","CaveSpiderFace","ChickenFace","CodFace","DolphinFace","DonkeyFace","DrownedFace","EndermanFace","EvokerFace","HoglinFace","IronGolemFace,"JebFace","PandaFace","PigFace","PiglinBruteFace","PillagerFace","PolarBearFace","PufferfishFace","MooshroomFace","SalmonFace","RabbitFace","SilverfishFace","FoxFace","SpiderFace","SquidFace","TadpoleFace","TropicalFishFace","TurtleFace","VexFace","VillagerFace","WanderingTraderFace","FrogFace","CatFace","AxolotlFace","WolfFace","EnderDragonFace","CowFace"];
 const charlieArray = [];
 const jackArray = [];
