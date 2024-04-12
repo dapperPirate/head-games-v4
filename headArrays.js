de4b9758df02d3b6c4f0e30ef1c02b0526d3502d
jtw26 <166263239+jtw26@users.noreply.github.com>
2024-04-12 12:52:59 +0100

    Update headArrays.js

--- headArrays.js
+++ headArrays.js
@@ -1,4 +1,4 @@
 const alexArray = [];
 const bobbieArray = ["bobbie","MiniBlockFace","BeeFace","BatFace","HorseFace","BlazeFace","ParrotFace","SheepFace","CamelFace","CaveSpiderFace","ChickenFace","CodFace","DolphinFace","DonkeyFace","DrownedFace","EndermanFace","EvokerFace","HoglinFace","IronGolemFace","PandaFace","PigFace","PiglinBruteFace","PillagerFace","PolarBearFace","PufferfishFace","MooshroomFace","SalmonBody","RabbitFace","SilverfishFace","FoxFace","SpiderFace","SquidFace","TadpoleFace","TropicalFishBody","TurtleFace","VexFace","VillagerFace","WanderingTraderFace","FrogFace","CatFace","AxolotlFace","WolfFace","EnderdragonFace","CowFace","JebSheepFace"];
-const charlieArray = [];
+const charlieArray = ["SheepFace","RabbitFace","CatFace","ParrotFace","OcelotFace","EvokerFace","WitchFace","IronGolemFace","AxolotlFace","PiglinBruteFace","VillagerFace","WitherFace","SilverFishFace","ZombieVillagerFace","PandaFace","EnderdragonFace","DonkeyFace","BeeFace","WolfFace","SquidFace","WanderingTraderFace","PufferFishFace","TraderLlama_Head","HorseFace","ChickenFace","PillagerFace","VindicatorFace"];
 const jackArray = [];
