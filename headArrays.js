cee96e4b9c591c50dba692aa5f473397a949cf93
jtw26 <166263239+jtw26@users.noreply.github.com>
2024-04-18 22:14:47 +0100

    Update headArrays.js

--- headArrays.js
+++ headArrays.js
@@ -1,5 +1,5 @@
 const alexArray = ["RabbitFace","WolfFace","ElderGuardianFace","SnowGolemFace",
-"EnderdragonFace","SalmonFace","PigFace","ParrotFace","SquidFace","TropicalFishFace",
+"EnderDragonFace","SalmonFace","PigFace","ParrotFace","SquidFace","TropicalFishFace",
 "VillagerFace","OcelotFace","CatFace","HorseFace","SheepFace","ZombieVillagerFace",
 "DonkeyFace","ShulkerFace","CodFace","WanderingTraderFace","EvokerFace","VindicatorFace",
 "BeeFace","ChickenFace","PhantomFace","MooshroomFace","PufferfishFace","HuskFace",
@@ -13,12 +13,12 @@ const bobbieArray = ["BeeFace","BatFace","HorseFace","BlazeFace","ParrotFace","S
 "HoglinFace","IronGolemFace","PandaFace","PigFace","PiglinBruteFace","PillagerFace","PolarBearFace",
 "PufferfishFace","MooshroomFace","SalmonFace","RabbitFace","SilverfishFace","FoxFace","SpiderFace","SquidFace",
 "TadpoleFace","TropicalFishFace","TurtleFace","VexFace","VillagerFace","WanderingTraderFace","FrogFace","CatFace",
-"AxolotlFace","WolfFace","EnderdragonFace","CowFace","LlamaFace","ZombieVillagerFace","EndermiteFace","StrayFace",
+"AxolotlFace","WolfFace","EnderDragonFace","CowFace","LlamaFace","ZombieVillagerFace","EndermiteFace","StrayFace",
 "VindicatorFace","ElderGuardianFace","JebSheepFace","PlayerFace","MiniBlock"];
 
 const charlieArray = ["SheepFace","RabbitFace","CatFace","ParrotFace","OcelotFace","EvokerFace","WitchFace",
 "IronGolemFace","AxolotlFace","PiglinBruteFace","VillagerFace","WitherFace","SilverfishFace","ZombieVillagerFace",
-"PandaFace","EnderdragonFace","DonkeyFace","BeeFace","WolfFace","SquidFace","WanderingTraderFace","PufferfishFace",
+"PandaFace","EnderDragonFace","DonkeyFace","BeeFace","WolfFace","SquidFace","WanderingTraderFace","PufferfishFace",
 "HorseFace","ChickenFace","PillagerFace","VindicatorFace","LlamaFace","CamelFace","GhastFace","FrogFace"];
 
-const jackArray = ["SquidFace","VillagerFace","SkeletonFace","PlayerFace","TropicalFishHead","ZombieVillagerHead","GlowSquidFace"];
+const jackArray = ["SquidFace","VillagerFace","SkeletonFace","PlayerFace","TropicalFishHead","ZombieVillagerFace","GlowSquidFace"];
